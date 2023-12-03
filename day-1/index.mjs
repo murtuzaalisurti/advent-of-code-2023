6cad632c4b0cd02b4b8102ff9030d5addf0ec8ce
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-12-03 13:17:31 +0530

    solved day-1 part-1

--- day-1/index.mjs
+++ day-1/index.mjs
@@ -22,25 +22,18 @@ async function readFileByLine() {
     const lines = await readFileByLine()
     const calibrationByLine = lines.map((line, i) => {
         const lineArr = line.split('')
-
         const firstAndLastNumber = {
             first: null,
             last: null
         }
 
         for (const [index, char] of lineArr.entries()) {
-
             if (!z.nan().safeParse(parseInt(char, 10)).success) {
-                if (firstAndLastNumber.first === null) {
-                    firstAndLastNumber.first = char
-                } else {
-                    firstAndLastNumber.last = char
-                }
+                firstAndLastNumber.first === null ? firstAndLastNumber.first = char : firstAndLastNumber.last = char;
             }
         };
 
         firstAndLastNumber.last === null && (firstAndLastNumber.last = firstAndLastNumber.first);
-        
         return parseInt(`${firstAndLastNumber.first}${firstAndLastNumber.last}`, 10)
     })
 
