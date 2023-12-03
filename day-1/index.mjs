a1d5a29edb9a71cc05d415db49258ef6bd636821
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-12-03 16:50:22 +0530

    completed day 1

--- day-1/index.mjs
+++ day-1/index.mjs
@@ -1,7 +1,10 @@
 import readline from 'readline'
 import fs from 'fs'
 import path from 'path'
-import {z} from 'zod'
+import { z } from 'zod'
+
+const regex = new RegExp('(?=(one|two|three|four|five|six|seven|eight|nine))', 'gm')
+const stringToIntMap = new Map([["one", 1], ["two", 2], ["three", 3], ["four", 4], ["five", 5], ["six", 6], ["seven", 7], ["eight", 8], ["nine", 9]])
 
 async function readFileByLine() {
     const lines = []
@@ -18,23 +21,51 @@ async function readFileByLine() {
 
     return lines
 }
+
 (async () => {
     const lines = await readFileByLine()
     const calibrationByLine = lines.map((line, i) => {
+        const allDigitsInLine = []
+
+        for (const match of line.matchAll(regex)) {
+            allDigitsInLine.push({
+                digit: stringToIntMap.get(match[1]),
+                index: match.index
+            })
+        }
+
         const lineArr = line.split('')
         const firstAndLastNumber = {
-            first: null,
-            last: null
+            first: {
+                digit: null,
+                index: null
+            },
+            last: {
+                digit: null,
+                index: null
+            }
         }
 
         for (const [index, char] of lineArr.entries()) {
             if (!z.nan().safeParse(parseInt(char, 10)).success) {
-                firstAndLastNumber.first === null ? firstAndLastNumber.first = char : firstAndLastNumber.last = char;
+                allDigitsInLine.push({
+                    digit: char,
+                    index
+                })
             }
         };
 
-        firstAndLastNumber.last === null && (firstAndLastNumber.last = firstAndLastNumber.first);
-        return parseInt(`${firstAndLastNumber.first}${firstAndLastNumber.last}`, 10)
+        const sortedDigitsAccordingToIndex = allDigitsInLine.sort((a, b) => a.index - b.index)
+
+        firstAndLastNumber.first = {
+            ...sortedDigitsAccordingToIndex[0]
+        }
+        firstAndLastNumber.last = {
+            ...sortedDigitsAccordingToIndex[sortedDigitsAccordingToIndex.length - 1]
+        }
+
+        firstAndLastNumber.last.digit === null && (firstAndLastNumber.last.digit = firstAndLastNumber.first.digit);
+        return parseInt(`${firstAndLastNumber.first.digit}${firstAndLastNumber.last.digit}`, 10)
     })
 
     const result = calibrationByLine.reduce((acc, curr) => acc + curr, 0)
