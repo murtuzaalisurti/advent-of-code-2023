d4a18d803efb96f528a292729bfd1db07ce0d96a
Murtuzaali Surti <murtuzaali.surti@imriel.com>
2023-12-08 13:26:49 +0530

    improved

--- day-2/index.mjs
+++ day-2/index.mjs
@@ -8,19 +8,19 @@ const totalCubesAtOnce = {
 
 const getCubeCountMappedToTheGameAndType = async () => {
     const games = await readFileByLine('day-2/input.txt')
-    const cubeCountMappedToTheGameAndType = []
-    
-    for (const game of games) {
+    const cubeCountMappedToTheGameAndType = Array.from({ length: games.length })
+
+    for (const [index, game] of games.entries()) {
         const splitGameNumberAndPlays = game.split(':').map(i => i.trim())
-    
+
         const cubesFromAPlay = {
             green: [],
             red: [],
             blue: []
         }
-    
+
         const plays = splitGameNumberAndPlays[1].split(';').map(i => i.trim())
-    
+
         for (const play of plays) {
             play.split(',').map(i => i.trim()).forEach((cubeType) => {
                 const cubeColor = cubeType.split(" ")[1];
@@ -28,11 +28,11 @@ const getCubeCountMappedToTheGameAndType = async () => {
                 cubesFromAPlay[`${cubeColor}`].push(cubeCount)
             })
         }
-    
-        cubeCountMappedToTheGameAndType.push({
+
+        cubeCountMappedToTheGameAndType[index] = {
             number: splitGameNumberAndPlays[0].split(" ")[1],
             cubes: cubesFromAPlay
-        })        
+        }
     }
     return cubeCountMappedToTheGameAndType
 }
@@ -44,7 +44,7 @@ const partOne = async () => {
     for (const game of cubeCountMappedToTheGameAndType) {
         let isValid = true
         loop2: for (const [type, value] of Object.entries(game.cubes)) {
-            loop3: for (const count of value) {
+            for (const count of value) {
                 if (parseInt(count) > totalCubesAtOnce[`${type}`]) {
                     isValid = false
                     break loop2;
@@ -54,29 +54,31 @@ const partOne = async () => {
 
         isValid && validGames.push(game);
     }
-    const sumOfValidGameNumbers = validGames.reduce((acc, curr) => acc + parseInt(curr.number), 0);
-    console.log(`\nsum of valid game numbers: `, sumOfValidGameNumbers);
+    return validGames.reduce((acc, curr) => acc + parseInt(curr.number), 0);
 }
 
 const partTwo = async () => {
     const cubeCountMappedToTheGameAndType = await getCubeCountMappedToTheGameAndType()
-    const powersOfCubes = []
-    
-    for (const game of cubeCountMappedToTheGameAndType) {
+    const powersOfCubes = Array.from({ length: cubeCountMappedToTheGameAndType.length })
+
+    for (const [i, game] of cubeCountMappedToTheGameAndType.entries()) {
         const powersOfCubeInAGame = [];
-        for (const [type, value] of Object.entries(game.cubes)) {
+        for (const [_, value] of Object.entries(game.cubes)) {
             powersOfCubeInAGame.push(value.sort((a, b) => parseInt(a) - parseInt(b))[value.length - 1])
         };
-        powersOfCubes.push(powersOfCubeInAGame.reduce((acc, curr) => acc * curr, 1))
+        powersOfCubes[i] = powersOfCubeInAGame.reduce((acc, curr) => acc * curr, 1)
     }
 
-    console.log(`sum of powers of min cubes`, powersOfCubes.reduce((acc, curr) => acc + curr, 0))
+    return powersOfCubes.reduce((acc, curr) => acc + curr, 0)
 }
 
 
 (async () => {
     console.time('exec');
-    await partOne()
-    await partTwo()
+    const sumOfValidGameNumbers = await partOne();
+    const sumOfPowersOfMinimumSetOfCubes = await partTwo();
     console.timeEnd('exec');
+
+    console.log(`\nsum of valid game numbers: `, sumOfValidGameNumbers);
+    console.log(`sum of powers of min cubes`, sumOfPowersOfMinimumSetOfCubes)
 })()
