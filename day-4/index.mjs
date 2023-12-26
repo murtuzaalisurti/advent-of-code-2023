a5fdc633904a9c0ab82db29eb5d353f215746907
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-12-26 17:16:02 +0530

    day 4

--- day-4/index.mjs
+++ day-4/index.mjs
@@ -1,76 +1,96 @@
 import { readFileByLine } from "../lib/shared.mjs";
 
-const numbers = new RegExp('\\d+', 'g')
 const lines = await readFileByLine("day-4/input.txt");
 
 const cardsWithTheirPoints = Array.from({ length: lines.length })
+const cardsWithTheirWinningCopies = Array.from({ length: lines.length }).map((_, i) => ({
+    card: i + 1,
+    copies: {
+        count: 1
+    }
+}))
 
-for (const [i, line] of lines.entries()) {
-    const cardNumbers = line.split(":")[1].trim()
-
-    const winningNumbers = cardNumbers.split("|")[0].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
-    const numbersIhave = cardNumbers.split("|")[1].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
-
-    let points = 0;
-    let firstMatch = true;
-    const matchingNumbers = []
-
-    for (const win of winningNumbers) {
-        if (numbersIhave.includes(win)) {
-            matchingNumbers.push(win)
-
-            if (!firstMatch) {
-                points = points * 2
-            } else {
-                points++;
+// Part 1
+function PartOne() {
+    for (const [i, line] of lines.entries()) {
+        const cardNumbers = line.split(":")[1].trim()
+    
+        const winningNumbers = cardNumbers.split("|")[0].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
+        const numbersIhave = cardNumbers.split("|")[1].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
+    
+        let points = 0;
+        let firstMatch = true;
+        const matchingNumbers = []
+    
+        for (const win of winningNumbers) {
+            if (numbersIhave.includes(win)) {
+                matchingNumbers.push(win)
+    
+                if (!firstMatch) {
+                    points = points * 2
+                } else {
+                    points++;
+                }
+    
+                firstMatch = false;
             }
-
-            firstMatch = false;
+    
         }
-
+    
+        cardsWithTheirPoints[i] = {
+            matchingNumbers,
+            points,
+            card: i + 1
+        }
     }
 
-    cardsWithTheirPoints[i] = {
-        matchingNumbers,
-        points,
-        card: i + 1
-    }
+    return cardsWithTheirPoints.reduce((acc, curr) => acc + curr.points, 0);
 }
 
-for (const [i, line] of lines.entries()) {
-    const card = i + 1;
-    const cardNumbers = line.split(":")[1].trim()
-
-    const winningNumbers = cardNumbers.split("|")[0].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
-    const numbersIhave = cardNumbers.split("|")[1].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
-
-    const matchingNumbers = []
-    const copiesOfCards = []
-
-    // part 2
-
-    for (const win of winningNumbers) {
-        if (numbersIhave.includes(win)) {
-            matchingNumbers.push(win)
+// Part 2
+function PartTwo() {
+    for (const [index, line] of lines.entries()) {
+        function calc(index, line) {
+            const card = index + 1;
+            const cardNumbers = line.split(":")[1].trim()
+            
+            const winningNumbers = cardNumbers.split("|")[0].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))
+            const numbersIhave = cardNumbers.split("|")[1].trim().split(" ").map(i => i.trim()).filter(j => parseInt(j)).map(k => parseInt(k))        
+            const matchingNumbers = []
+            
+        
+            for (const win of winningNumbers) {
+                if (numbersIhave.includes(win)) {
+                    matchingNumbers.push(win)
+                }
+            }
+        
+            if (matchingNumbers.length > 0) {            
+                const nextcards = Array.from({length: ((matchingNumbers.length + card) - card)/1 + 1}, (_, t) => card + t * 1).slice(1);
+    
+                for (const winningCopyNumber of nextcards) {
+                    cardsWithTheirWinningCopies[winningCopyNumber - 1].copies.count++;
+                }
+            }
+        }
+    
+        for (let copy = 0; copy < cardsWithTheirWinningCopies[index].copies.count; copy++) {
+            calc(index, line);
         }
     }
 
-    if (matchingNumbers.length > 0) {
-        console.log(`\nlength: `, matchingNumbers.length)
-        console.log(`card: `, card)
-
-        copiesOfCards.push({
-            [card]: 1
-        })
-
-        for (const numbers of Array.from({length: ((matchingNumbers.length + card) - card)/1 + 1}, (_, i) => card + i * 1)) {
-            console.log(numbers)
-        }
-    }
+    return cardsWithTheirWinningCopies.reduce((acc, curr) => curr.copies.count + acc, 0);
 }
 
-console.time('exec')
-const totalPointsOfPileOfCards = cardsWithTheirPoints.reduce((acc, curr) => acc + curr.points, 0);
-console.timeEnd('exec')
 
-// console.log(totalPointsOfPileOfCards)
+
+console.time('exec - Part 1')
+const totalPointsOfPileOfCards = PartOne();
+console.timeEnd('exec - Part 1')
+
+console.time('exec - Part 2')
+const totalWonScratchCards = PartTwo();
+console.timeEnd('exec - Part 2');
+
+console.log(totalPointsOfPileOfCards)
+console.log(totalWonScratchCards)
